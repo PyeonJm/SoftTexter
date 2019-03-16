ae4ca915fc2369a49a4783787d629faeff6c90db
abzy12 <abzy5203060@gmail.com>
2019-03-17 04:29:56 +0900

    ++

--- Hello.c
+++ Hello.c
@@ -2,12 +2,16 @@
 
 int main(void)
 {
-    printf("practice\n");
-    printf("my time is %d\n", 30);
-    printf("my number is %d, %d\n", 6, 44);
-    
-    printf("%d x %d = %d\n", 2, 3, 2*3);
-    printf("%d x %d = %d\n", 2, 4, 2*4);
+
+    int val3= 10;                // 10, 11, 12
+    int val4= (val3++)+(++val3); // 10 + 12 = 22, 23, 24
+    int val5= (++val4)+(val4++); // 23 + 24 = 47, 48, 49
+    int val6= (val5++)+(++val5); // 47 + 49 = 96
+
+    printf(" val3 : %d \n", val3);
+    printf(" val4 : %d \n", val4);
+    printf(" val5 : %d \n", val5);
+    printf(" val6 : %d \n", val6);
 
     return 0;
 }

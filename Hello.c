0e52990496ee1d1d7b51070bdaac3cc7e9d4b65f
abzy12 <abzy5203060@gmail.com>
2019-03-17 05:20:06 +0900

    Operator

--- Hello.c
+++ Hello.c
@@ -2,16 +2,42 @@
 
 int main(void)
 {
+    int val1=10;
+    int val2=12;
+    int result1, result2, result3;
 
-    int val3= 10;                // 10, 11, 12
-    int val4= (val3++)+(++val3); // 10 + 12 = 22, 23, 24
-    int val5= (++val4)+(val4++); // 23 + 24 = 47, 48, 49
-    int val6= (val5++)+(++val5); // 47 + 49 = 96
+    int val3=10;
+    int val4=12;
+    int val5=0;
+    int val6=2;
+    int result4, result5, result6, result7;
 
-    printf(" val3 : %d \n", val3);
-    printf(" val4 : %d \n", val4);
-    printf(" val5 : %d \n", val5);
-    printf(" val6 : %d \n", val6);
+    int a=1, b=2;
+    
+    //==, >, <, >=, <= true = 1, False = 0.
+    result1=(val1==val2); //X = 0
+    result2=(val1<=val2); //O = 1
+    result3=(val1>val2);  //X = 0
 
+    printf("result1 : %d \n", result1);
+    printf("result2 : %d \n", result2);
+    printf("result3 : %d \n\n", result3);
+
+    //&& = and, || = or,  ! = 0->1, 1->0 
+    result4= (val3==10 && val4==12); //O = 1
+    result5= (val3<12 || val4>12);   //O = 1
+    result6= (!val5);                //O = 1
+    result7= (!val6);                //X = 0
+
+    printf("result4 : %d \n", result4);
+    printf("result5 : %d \n", result5);
+    printf("result6 : %d \n", result6);
+    printf("result7 : %d \n\n", result7);
+
+    //Comma, Practice
+    a++, --b, --a, b++; //a=1, 1+1-1, b=2, 2-1+1 
+    printf("Comma "), printf("Practice\n"); //'Comma '+'Practice'+'\n' 
+    printf("%d ", a), printf("%d ", b); //1 2 
+    
     return 0;
 }

4583d106936ce2665e376f3363ecffa1f82c9cd0
abzy12 <abzy5203060@gmail.com>
2019-03-19 10:59:40 +0900

    Bit Operator

--- Hello.c
+++ Hello.c
@@ -2,42 +2,22 @@
 
 int main(void)
 {
-    int val1=10;
-    int val2=12;
-    int result1, result2, result3;
+    int a=15, b=20;  
+    // a = 00000000 00000000 00000000 00001111
+    // b = 00000000 00000000 00000000 00010100
+    int B_and= a&b; 
+    int B_or= a|b;
+    int B_xor= a^b;
+    int B_not= ~a;
+    int B_Lshift= a<<2;
+    int B_Rshift= a>>2;
 
-    int val3=10;
-    int val4=12;
-    int val5=0;
-    int val6=2;
-    int result4, result5, result6, result7;
+    printf("AND         &  : %d\n", B_and);              //4
+    printf("OR          |  : %d\n", B_or);               //31
+    printf("XOR         ^  : %d\n", B_xor);              //27
+    printf("NOT         ~  : %d\n", B_not);              //-16
+    printf("LEFT SHIFT  << : %d\n", B_Lshift);           //60
+    printf("RIGHT SHIFT >> : %d\n", B_Rshift);           //3
 
-    int a=1, b=2;
-    
-    //==, >, <, >=, <= true = 1, False = 0.
-    result1=(val1==val2); //X = 0
-    result2=(val1<=val2); //O = 1
-    result3=(val1>val2);  //X = 0
-
-    printf("result1 : %d \n", result1);
-    printf("result2 : %d \n", result2);
-    printf("result3 : %d \n\n", result3);
-
-    //&& = and, || = or,  ! = 0->1, 1->0 
-    result4= (val3==10 && val4==12); //O = 1
-    result5= (val3<12 || val4>12);   //O = 1
-    result6= (!val5);                //O = 1
-    result7= (!val6);                //X = 0
-
-    printf("result4 : %d \n", result4);
-    printf("result5 : %d \n", result5);
-    printf("result6 : %d \n", result6);
-    printf("result7 : %d \n\n", result7);
-
-    //Comma, Practice
-    a++, --b, --a, b++; //a=1, 1+1-1, b=2, 2-1+1 
-    printf("Comma "), printf("Practice\n"); //'Comma '+'Practice'+'\n' 
-    printf("%d ", a), printf("%d ", b); //1 2 
-    
     return 0;
 }
